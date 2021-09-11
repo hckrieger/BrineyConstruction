f5df20576e680298134d7d25a5e4de0f92412637
Hunter Krieger <hunterk31@hotmail.com>
2021-09-11 15:30:31 -0400

    first commit

---
+++ BrineyConstruction/Data/Migrations/20210911154730_AddIndentityToDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BrineyConstruction.Data.Migrations
+{
+    public partial class AddIndentityToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
